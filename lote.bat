dff031d091d66cfd07faf283d8b59d5cd51b9f29
Alicia <lvilchezo@unprg.edu.pe>
2019-12-08 01:51:21 -0500

    condicional simple 03

--- lote.bat
+++ lote.bat
@@ -8,4 +8,10 @@ python Vilchez/simple02.py "Patrick" 28 75 10
 python Vilchez/simple02.py "Keyla" 27 65 5
 
 
+python Vilchez/simple03.py "Rita" 2
+python Vilchez/simple03.py "Carlos" 4
+python Vilchez/simple03.py "Jose" 5
+
+
+
 
